9af9783f51e33be967ee4426ea65dc17ac5f8fbc
Loc Nguyen Huu <18050078@student.bdu.edu.vn>
2022-05-02 14:38:05 +0700

    Edited config database at 2:37pm 02/05/2022

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const path = require("path");
+const { dirname } = require('path');
 dotenv.config();
 
 async function connect() {
   try {
-    await mongoose.connect(process.env.CONNECTION_STRING);
+    // Lấy đường dẫn tới file ca-certificate.crt
+    const mongoCertPath = path.join(__dirname, 'ca-certificate.crt');
+
+    await mongoose.connect(
+      process.env.DB_URL, {
+      authSource: 'admin',
+      replicaSet: 'db-lobo',
+      tls: true,
+      tlsCAFile: mongoCertPath
+    })
     console.log('Connect successfully');
   } catch (error) {
     console.log('Connect failure');
