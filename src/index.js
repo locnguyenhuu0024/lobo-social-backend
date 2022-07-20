14785c102f60d6b86dee1968b9a045474c2f91ec
Loc Nguyen Huu <18050078@student.bdu.edu.vn>
2022-07-20 13:12:05 +0700

    can loi

--- src/index.js
+++ src/index.js
@@ -33,6 +33,11 @@ app.use(
 app.use(express.json());
 app.use(morgan('combined'));
 app.use(methodOverride('_method'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Setup view engine
 app.engine(
